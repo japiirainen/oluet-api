d5bae3e8b80b765b386b17668a8337b4019eae4e
japiirainen <joona.piirainen@gmail.com>
2020-10-21 18:21:07 +0300

    format file

--- src/utils/readExel.ts
+++ src/utils/readExel.ts
@@ -12,4 +12,19 @@ export const readXlsx = async (path: string): Promise<Workbook | void> => {
    }
 }
 
-console.log(readXlsx(ALKO_FILE).then(console.log))
+const formatXlsx = async (path: string) => {
+    const fileContents = await readXlsx(path)
+    if (!fileContents) {
+        console.error('reading file failed')
+        return
+    } else {
+       return {
+        created: fileContents.created,
+        nimi: "joona"
+    }
+}
+}
+
+const res = formatXlsx(ALKO_FILE)
+console.log(res)
+
